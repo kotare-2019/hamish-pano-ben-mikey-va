f163985dc86a24f6fb5e4bab4b6c4d3ab7d98a37
Pano Skylakis <49353965+pano-skylakis@users.noreply.github.com>
2019-06-28 10:19:03 +1200

    bug fix - random 1 in migrations

--- seeds/beer_locations.js
+++ seeds/beer_locations.js
@@ -1,11 +1,11 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('beer_locations').del()
     .then(function () {
       // Inserts seed entries
       return knex('beer_locations').insert([
-        {name: 'Bay 66', type: 'pub', descript: 'Hive of scum and villany', rating: 1, lat_long_wsg84: '[-41.3195036,174.7943524]', url: 'https://gph.is/2qvUYQk'},
+        { name: 'Bay 66', type: 'pub', descript: 'Hive of scum and villany', rating: 1, lat_long_wsg84: '[-41.3195036,174.7943524]', url: 'https://gph.is/2qvUYQk' },
       ])
     })
 }
