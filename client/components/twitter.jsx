71d7c53284f426cfd7c3e8ad728603802c1767f7
mikeywipo <mikewipa@gmail.com>
2019-06-28 10:11:40 +1200

    add components

---
+++ client/components/twitter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Slider from './Slider'
+
+const App = () => {
+  return (
+    <div>
+    <h1>Un-Beer-Able</h1>
+    <Slider />
+    </div>
+  )
+}
+
+export default App
