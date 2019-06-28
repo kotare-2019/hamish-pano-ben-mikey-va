35813d5e4e74a87eb67c6000aff18c8b66446e12
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-06-28 12:50:44 +1200

    data accessible from front page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import Slider from './Slider'
 import Instagram from './instagram'
 import Facebook from './facebook'
 import Twitter from './twitter'
+import { getBeers } from '../api/api'
 
 
 class App extends React.Component {
@@ -13,22 +14,28 @@ class App extends React.Component {
 
     }
   }
-  
+
+  handleClick() {
+    getBeers()
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div className="logo">  
-        <ul className="social">
-          <li><Instagram /></li>
-          <li><Facebook /></li>
-          <li><Twitter /></li>
-        </ul>
-      </div>
-      <h1>Un-Beerable</h1>
-      <h2>"for all the best bad beers"</h2>
-      <h2>Choose a beer to rate</h2>
-      <Slider />
-  
+        <a href="#" onClick={this.handleClick}>Click this bitch</a>
+
+        <div className="logo">
+          <ul className="social">
+            <li><Instagram /></li>
+            <li><Facebook /></li>
+            <li><Twitter /></li>
+          </ul>
+        </div>
+        <h1>Un-Beerable</h1>
+        <h2>"for all the best bad beers"</h2>
+        <h2>Choose a beer to rate</h2>
+        <Slider />
+
       </React.Fragment>
     )
   }
