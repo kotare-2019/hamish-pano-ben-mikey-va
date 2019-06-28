520f7b34b02a78fd016a178cb08c7533f69a67a5
Pano Skylakis <49353965+pano-skylakis@users.noreply.github.com>
2019-06-28 14:19:12 +1200

    beer data

--- client/components/Slider.jsx
+++ client/components/Slider.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
 
-const App = () => {
+export default function Slider(props) {
+  console.log(props)
   return (
-    <div>
-      <section id="slider">
-            <input type="radio" name="slider" id="s1"/>
-            <input type="radio" name="slider" id="s2"/>
-            <input type="radio" name="slider" id="s3"/>
-            <input type="radio" name="slider" id="s4"/>
-            <input type="radio" name="slider" id="s5"/>
-            <label for="s1" id="slide1"></label>
-            <label for="s2" id="slide2"></label>
-            <label for="s3" id="slide3"></label>
-            <label for="s4" id="slide4"></label>
-            <label for="s5" id="slide5"></label>
+    <React.Fragment>
+      <section id='slider'>
+        {this.props.beers.map(beer => {
+          return <input type="radio" name="slider" id={beer.id} />
+        })}
+        {/* <section id="slider">
+          <input type="radio" name="slider" id="s1" />
+          <input type="radio" name="slider" id="s2" />
+          <input type="radio" name="slider" id="s3" />
+          <input type="radio" name="slider" id="s4" />
+          <input type="radio" name="slider" id="s5" /> */}
+        <label for="s1" id="slide1"></label>
+        <label for="s2" id="slide2"></label>
+        <label for="s3" id="slide3"></label>
+        <label for="s4" id="slide4"></label>
+        <label for="s5" id="slide5"></label>
       </section>
-    </div>
+    </React.Fragment >
   )
 }
 
-export default App
+
