d2bb4840ee1b7fb78a1066cc7ffd5e643156bc6e
mikeywipo <mikewipa@gmail.com>
2019-06-28 14:13:19 +1200

    adding margin to text

--- client/components/BeerDetails.jsx
+++ client/components/BeerDetails.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-        <div className="beerdetails">hello</div>
+        <div className="beerdetails">
+        <p>brewery_id: 3, name: 'Rheineck', style: 'beer', abv: 3.5, descript: 'Brewed in NZ, but cunningly passed off as a german beer probably to annoy german tourists(though the city of Rheineck is in Switzerland). In terms of flavour, opinions are split. Witty commentators have stated "I will give it a 3 for appearance because, well, it looks like a beer. Apart from that if a frog, a goat and a monkey vomited into a cup I would rather drink that than Rheineck." Most would say at 3.5%ABV whats the point?', rating: 1, loc_available: '[1]', url: 'images/Rheineck.jpeg' },
+        </p>            
+        </div>
     </div>
   )
 }
